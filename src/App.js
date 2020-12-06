88a78af4a95baabefc88181902585d37b4f90b71
Magiskandev <hello@mail.com>
2020-12-06 16:39:34 -0500

    added render function

--- src/App.js
+++ src/App.js
@@ -39,8 +39,25 @@ fetch(API).then(response => response.json()).then(data =>{
   render(){
     let twitterUrl = `https://twitter.com/intent/tweet?text=${this.state.quoteText}- ${this.state.quoteAuthor}`;
     return (<div id="quote-box">
+                <div id="app-title">
+                  <h1>Random Quote Machine</h1>
+                </div>
+                <div id="quote-text">
+                    <i class="fa fa-quote-left quote-tag" style={{color:this.state.color}}></i>
+                    <h2 id="text" style={{color:this.state.color}}>{this.state.quoteText}</h2>
+                </div>
+                <div id="quote-author">
+                    <cite id="author" style={{color:this.state.color}}>-{this.state.quoteAuthor}</cite>
+                </div>
+                <div id="tweet-newQuote">
+                    <a id="tweet-quote" href={twitterUrl}  target="_blank" rel="noreferrer">
+                      <i class="fa fa-twitter"></i>
+                    </a>
+                    <button id="new-quote" onClick={this.handlenewQuote}>New Quote</button>
+                </div>
+            </div>)
+  }
   
-
-
+}
 
 export default App;
