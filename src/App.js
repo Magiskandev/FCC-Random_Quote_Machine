65e96b40d883e2f5a3e9cf8922c5fbbdbe4bde7e
Magiskandev <hello@mail.com>
2020-12-06 21:59:10 -0500

    styles added.

--- src/App.js
+++ src/App.js
@@ -13,10 +13,11 @@ class App extends React.Component{
     }
     this.handlenewQuote = this.handlenewQuote.bind(this);
 }
+  
   componentDidMount() {
-const API = "https://raw.githubusercontent.com/Magiskandev/testa/master/quotes.json";
-console.log(API)
-fetch(API).then(response => response.json()).then(data =>{
+  const API = "https://raw.githubusercontent.com/Magiskandev/testa/master/quotes.json";
+
+  fetch(API).then(response => response.json()).then(data =>{
       this.setState({
         apiResult: data.quotes,
         quoteText: data.quotes[0].quote,
@@ -27,10 +28,10 @@ fetch(API).then(response => response.json()).then(data =>{
   }
     
     handlenewQuote() {
-    const color = ['#16a085','#27ae60','red','#f39c12','#e74c3c','#9b59b6','#FB6964',
-                    'brown','green','yellow','blue','#73A857'];
-    let randomNumber = Math.floor(Math.random()*(this.state.quoteArrayLength));
-    let randomColorNumber = Math.floor(Math.random()*(this.state.color.length));
+    const color = ['red', '#27ae60', '#9b59b6', '#f39c12', '#e74c3c', '#FB6964', 'green', 'yellow', 'lightblue', '#73A857', 'teal', 'blue'];
+
+    let randomNumber = Math.trunc(Math.random()*(this.state.quoteArrayLength));
+    let randomColorNumber = Math.trunc(Math.random()*(this.state.color.length));
     this.setState({
       quoteText: this.state.apiResult[randomNumber].quote,
       quoteAuthor: this.state.apiResult[randomNumber].author,
@@ -39,23 +40,24 @@ fetch(API).then(response => response.json()).then(data =>{
   }
 
   render() {
-    let twitterUrl = `https://twitter.com/intent/tweet?text=${this.state.quoteText}- ${this.state.quoteAuthor}`;
+    let twitterUrl = `https://twitter.com/intent/tweet?text=${this.state.quoteText} - ${this.state.quoteAuthor}`;
     return (<div id="quote-box">
                 <div id="app-title">
-                  <h1>Random Quote Machine</h1>
+                  <h1>FCC Random Quote Machine</h1>
                 </div>
                 <div id="quote-text">
                     <i className="fa fa-quote-left quote-tag" style={{color:this.state.color}}></i>
-                    <h2 id="text" style={{color:this.state.color}}>{this.state.quoteText}</h2>
+                    <h2 id="text" style={{color:this.state.color}}>{this.state.quoteText}.</h2>
+                    <i className="fa fa-quote-right quote-tag" style={{color:this.state.color}}></i>
                 </div>
                 <div id="quote-author">
                     <cite id="author" style={{color:this.state.color}}>-{this.state.quoteAuthor}</cite>
                 </div>
                 <div id="tweet-newQuote">
                     <a id="tweet-quote" href={twitterUrl}  target="_blank" rel="noreferrer">
-                      <i class="fa fa-twitter"></i>
+                      <i className="fa fa-twitter"></i>
                     </a>
-                    <button id="new-quote" onClick={this.handlenewQuote}>New Quote</button>
+                    <button id="new-quote" onClick={this.handlenewQuote}>Siguiente Cita</button>
                 </div>
             </div>)
   }
